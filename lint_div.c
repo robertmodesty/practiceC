2397d8f67fe77edbeaa96a8844d07bef2c6e9d3f
Robert D. Wei <robertmodesty@gmail.com>
2015-07-23 16:10:10 +0800

    update for lint_div.c, file time:  1999-05-14 20:51:02 +0800

--- lint_div.c
+++ lint_div.c
@@ -30,7 +30,7 @@ void main () {
 	  }
 	}
 
-	Sh = 0xfc;
+	Sh = 0xf0;
 	for (A = 0; A < 0xe0000000; A = Sh) {
 	  for (Sk = A + 500; A < Sk; A++) {
 	    for (B = 1 ; (int)(B/16777216) < 255; B += Rndm) {
@@ -46,7 +46,7 @@ void main () {
 	  }
 	  printf("#%2d%04d%04d\n", (int)(A/100000000),
 	      (int)(A/10000%10000), (int)(A%10000));
-	  if (Sh < 0x20000000) Sh = Sh * 2 + 4;
+	  if (Sh < 0x20000000) Sh = Sh * 2 + 16;
 	  else Sh += 0x20000000;
 	}
 	printf("OK!\n");
@@ -217,7 +217,7 @@ char LInt_Div (unsigned char *A, unsigned char *B,
 		*(OddB + Cnt + Shft + 1) = (char) (ASub >> 8);
 		ASub = (ASub & 0xff) << 8;
 	      }
-	      /* Odd is too less, decress the maximum of quotient.*/
+	      /* Sub number not enough, get from high byte. */
 	      else {
 		for (Cnt1 = Cnt + Shft + 2;
 		      Cnt1 <= OLth && *(OddB + Cnt1) == (char) 0;
@@ -230,6 +230,7 @@ char LInt_Div (unsigned char *A, unsigned char *B,
 		  *(OddB + Cnt1) = (char) (ASub >> 8);
 		  ASub = (ASub & 0xff) << 8;
 		}
+		/* Odd is too less, decress the maximum of quotient. */
 		else {
 		  QuoSub -= (QuoSub - QuoSubB + 1) >> 1;
 		  RtCod = (char) 1;
@@ -275,6 +276,142 @@ char LInt_Div (unsigned char *A, unsigned char *B,
 	return (char) 0;
 	}
 
+char LInt_Div2 (unsigned char *A, unsigned char *B,
+		unsigned char *Quo, unsigned char *Odd) {
+	unsigned char Odd1 [256], Odd2[256], *OddA, *OddB, *Tmp, RtCod;
+	int Cnt, Cnt1, Shft;
+	unsigned int OSub, DivSub, QuoSub, BLth, OLth, QuoSubB;
+	char LInt_Add (unsigned char *, unsigned char *);
+
+	/* Find the length of B (divider). */
+	for (BLth = (int) *B;
+	      BLth != 0 && *(B + BLth) == (char) 0;
+	       BLth--) ;
+	/* Error! Divided by zero. */
+	if (BLth == 0) {
+	  *Odd = (char) 0;
+	  *Quo = (char) 0;
+	  return (char) 1;
+	}
+
+	/* Find the length of A (dividend). */
+	for (OLth = (int) *A;
+	      OLth != 0 && *(A + OLth) == (char) 0;
+	       OLth--) ;
+	/* Initialize Quotient and Odd. */
+	*Quo = (char) 0;
+	for (Cnt = 1; Cnt <= OLth; Cnt++) *(Odd + Cnt) = *(A + Cnt);
+	Odd1 [0] = (char) OLth;
+
+	/* Complite dividing if length of A < length of B (in bit). */
+	if ((OLth < BLth) ||
+	    ((OLth == BLth) && (*(A + OLth) < *(B + BLth)))) {
+	  for (Cnt = 0; Cnt <= OLth; Cnt++) *(Odd + Cnt) = Odd1 [Cnt];
+	  return (char) 0;
+	}
+
+	/* Point the Odd. */
+	OddA = Odd1;
+	OddB = Odd2;
+
+	do {
+	  /* Calculite the maximum and minimum of quotient. */
+	  QuoSubB = (int) *(OddA + OLth) / ((int) *(B + BLth) + 1);
+	  QuoSub = (int) *(OddA + OLth) / (int) *(B + BLth);
+
+	  /* Divide with one byte quotient. */
+	  RtCod = (char) 0;
+	  do {
+	    /* Divide the highest byte. */
+	    if (QuoSub != 0) {
+	      Shft = OLth - BLth;
+	      OSub = (int) *(OddA + OLth);
+	      DivSub = (int) *(B + BLth) * QuoSub;
+	      OSub -= DivSub;
+	      OSub <<= 8;
+	    }
+	    /* Number in highest byte is too less, get the second byte */
+	    /* or complite dividing if Odd is not enough. */
+	    else if (OLth == BLth) RtCod = (char) 2;
+	    else {
+	      QuoSub = (int) *(unsigned short *) (OddA + OLth - 1);
+	      OSub = QuoSub;
+	      QuoSub /= ((int) *(B + BLth) + 1);
+	      Shft = OLth - BLth - 1;
+	      DivSub = (int) *(B + BLth) * QuoSub;
+	      OSub -= DivSub;
+	      *(OddB + OLth) = (char) (OSub >> 8);
+	      OSub = (OSub & 0xff) << 8;
+	    }
+
+	    /* Divide the remaining if RtCod != 2. */
+	    if (RtCod == (char) 1) RtCod = (char) 0;
+	    for (Cnt = BLth - 1; Cnt > 0 && RtCod == (char) 0; Cnt--) {
+	      OSub += (int) *(OddA + Cnt + Shft);
+	      DivSub = (int) *(B + Cnt) * QuoSub;
+	      /* Divide if the Odd is still enough. */
+	      if (OSub >= DivSub) {
+		OSub -= DivSub;
+		*(OddB + Cnt + Shft + 1) = (char) (OSub >> 8);
+		OSub = (OSub & 0xff) << 8;
+	      }
+	      /* Sub number not enough, get from high byte. */
+	      else {
+		for (Cnt1 = Cnt + Shft + 2;
+		      Cnt1 <= OLth && *(OddB + Cnt1) == (char) 0;
+		       Cnt1++) ;
+		if (Cnt1 <= OLth) {
+		  *(OddB + Cnt1) -= 1;
+		  for (Cnt1-- ; Cnt1 >= Cnt + Shft + 2; Cnt1--)
+		    *(OddB + Cnt1) = (char) 0xff ;
+		  OSub += (0xffff - DivSub) + 1;
+		  *(OddB + Cnt1) = (char) (OSub >> 8);
+		  OSub = (OSub & 0xff) << 8;
+		}
+		/* Odd is too less, decress the maximum of quotient. */
+		else {
+		  QuoSub -= (QuoSub - QuoSubB + 1) >> 1;
+		  RtCod = (char) 1;
+		}
+	      }
+	    }
+
+	    /* If one byte diveding complite normally, correct the Odd. */
+	    if (RtCod == (char) 0) {
+	      *(OddB + Shft + 1) = (char) (OSub >> 8);
+
+	      /* Copy the part of Odd that been or did not divided. */
+	      if (((int) OddA - Shft) <= Shft) {
+		for (Cnt = Shft; Cnt >= 0; Cnt--)
+		  *(OddB + Cnt) = *(OddA + Cnt);
+		Tmp = OddB;
+		OddB = OddA;
+		OddA = Tmp; }
+	      else
+		for (Cnt = (int) *OddA; Cnt > Shft; Cnt--)
+		  *(OddA + Cnt) = *(OddB + Cnt);
+
+	      /* Correct the length. */
+	      for (OLth = (int) *OddA;
+		    OLth != 0 && *(OddA + OLth) == (char) 0;
+		     OLth--) ;
+	      *OddA = (char) OLth;
+
+	      /* Add the one byte quotient into Quotient.*/
+	      *OddB = (char) (Shft + 1);
+	      for (Cnt = 1; Cnt <= Shft; Cnt++) *(OddB + Cnt) = (char) 0;
+	      *(OddB + Cnt) = QuoSub;
+	      LInt_Add (Quo, OddB);
+
+	      /* Complite dividing if Odd is too less. */
+	      if (OLth >= BLth) RtCod = (char) 0;
+	      else RtCod = (char) 2; }
+	  } while (RtCod == (char) 1);
+	} while (RtCod == (char) 0);
+
+	for (Cnt = 0; Cnt <= OLth; Cnt++) *(Odd + Cnt) = *(OddA + Cnt);
+	return (char) 0;
+	}
 
 char LInt_Add (unsigned char *X1, unsigned char *X2) {
 	int X1Sub, Cnt, BigLth, SmlLth;
